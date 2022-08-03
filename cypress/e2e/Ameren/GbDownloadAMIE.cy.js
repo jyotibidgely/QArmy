debe5c856b79e6bd24fa0f5864790e7ee631ad9a
Gourav <gouravagrawal@bidgely.com>
2022-08-03 22:59:54 +0530

    added AMR E test

--- cypress/e2e/Ameren/GbDownloadAMIE.cy.js
+++ cypress/e2e/Ameren/GbDownloadAMIE.cy.js
@@ -16,6 +16,7 @@ describe("GB download", () => {
     var newEpochEndTs
     var arrValues = []
     var strObj = ''
+    var meterToken
     const uuid = '00fb4a7d-77b6-4667-b39d-4a420b2638e5'
     var bearerToken
     var userHash
@@ -24,17 +25,18 @@ describe("GB download", () => {
     before(function () {
         cy.getAccessToken().then((token) => {
             bearerToken = token
+            cy.log(bearerToken)
             cy.request({
                 method: 'GET',
-                url: baseUrl+'/v2.0/user-auth/cipher?user-id='+uuid+'&pilot-id='+pilotData.pilotId,
-                headers: { 'Authorization': 'Bearer '+bearerToken }, timeout: 30000
+                url: baseUrl + '/v2.0/user-auth/cipher?user-id=' + uuid + '&pilot-id=' + pilotData.pilotId,
+                headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
             })
                 .then((Response) => {
                     expect(Response.status).to.eq(200)
                     let res = Response.body
                     cy.log(res.payload)
                     userHash = res.payload
-                    cy.visit(pilotData.url+"dashboard?user-hash="+res.payload)
+                    cy.visit(pilotData.url + "dashboard?user-hash=" + res.payload)
                 })
         })
     })
@@ -42,41 +44,41 @@ describe("GB download", () => {
         cy.log(bearerToken)
         cy.request({
             method: 'GET',
-            url: baseUrl+'/billingdata/users/'+uuid+'/homes/1/utilitydata?t0=1&t1=1906986799&measurementType=ELECTRIC',
-            headers: { 'Authorization': 'Bearer '+bearerToken }, timeout: 30000
+            url: baseUrl + '/billingdata/users/' + uuid + '/homes/1/utilitydata?t0=1&t1=1906986799&measurementType=ELECTRIC',
+            headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
         })
             .then((Response) => {
                 expect(Response.status).to.eq(200)
                 let res = Response.body
                 cy.log(res)
                 var objLength = Object.keys(res).length;
-                var firstKey = Object.keys(res)[objLength-1];
+                var firstKey = Object.keys(res)[objLength - 1];
                 let objData = res[firstKey]
                 cy.log(objData)
                 billingEndTs = objData['billingEndTs']
                 billingStartTs = objData['billingStartTs']
                 cy.log(billingStartTs)
                 cy.log(billingEndTs)
-                var startTs = new Date(billingStartTs*1000); // The 0 there is the key, which sets the date to the epoch
+                var startTs = new Date(billingStartTs * 1000); // The 0 there is the key, which sets the date to the epoch
                 // d.setUTCSeconds(billingEndTs);
-                var endTs = new Date(billingEndTs*1000);
+                var endTs = new Date(billingEndTs * 1000);
                 endTs.setDate(endTs.getDate() - 1)
                 cy.log(startTs.toLocaleString())
                 cy.log(endTs.toDateString())
                 strStartDate = objGenericPage.changDateFormat(startTs.toDateString())
                 strEndDate = objGenericPage.changDateFormat(endTs.toDateString())
-                cy.log('Start date - '+strStartDate)
+                cy.log('Start date - ' + strStartDate)
                 cy.wrap(strStartDate).as('strStartDate');
 
-                var newStartTs = new Date(billingEndTs*1000);
+                var newStartTs = new Date(billingEndTs * 1000);
                 newStartTs.setDate(newStartTs.getDate() - 10)
                 strNewStartDate = objGenericPage.changDateFormat(newStartTs.toDateString())
-                newEpochStartTs = newStartTs.getTime()/1000
+                newEpochStartTs = newStartTs.getTime() / 1000
 
-                var newEndTs = new Date(billingEndTs*1000);
+                var newEndTs = new Date(billingEndTs * 1000);
                 newEndTs.setDate(newEndTs.getDate() - 9)
                 strNewEndDate = objGenericPage.changDateFormat(newEndTs.toDateString())
-                newEpochEndTs = newEndTs.getTime()/1000
+                newEpochEndTs = newEndTs.getTime() / 1000
             })
     })
 
@@ -116,7 +118,7 @@ describe("GB download", () => {
         objGbDownload.checkSuccessMsg('GreenButton data downloaded successfully.')
     })
     it("Export data - Days", () => {
-        cy.log('Start date - '+strStartDate)
+        cy.log('Start date - ' + strStartDate)
 
         cy.contains('Export usage for range of days').click()
         objGbDownload.enterFromDate('16/09/20')
@@ -156,8 +158,8 @@ describe("GB download", () => {
     it('Fetch values from RAW data', () => {
         cy.request({
             method: 'GET',
-            url: baseUrl+'/streams/users/'+uuid+'/homes/1/gws/2/meters/1/gb.json?t0='+newEpochStartTs+'&t1='+newEpochEndTs,
-            headers: { 'Authorization': 'Bearer '+bearerToken }, timeout: 30000
+            url: baseUrl + '/streams/users/' + uuid + '/homes/1/gws/2/meters/1/gb.json?t0=' + newEpochStartTs + '&t1=' + newEpochEndTs,
+            headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
         })
             .then((Response) => {
                 expect(Response.status).to.eq(200)
@@ -175,8 +177,25 @@ describe("GB download", () => {
                     const duration = element['duration']
                     value = Math.round(value)
                     cy.log(value)
-                    strObj = strObj + '<espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>'+duration+'</espi:duration><espi:start>'+time+'</espi:start></espi:timePeriod><espi:value>'+value+'</espi:value></espi:IntervalReading>'
+                    strObj = strObj + '<espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>' + duration + '</espi:duration><espi:start>' + time + '</espi:start></espi:timePeriod><espi:value>' + value + '</espi:value></espi:IntervalReading>'
                 }
+            })
+    })
+
+    it('Fetch values from Meter API', () => {
+        cy.request({
+            method: 'GET',
+            url: baseUrl + '/meta/users/' + uuid + '/homes/1/gws/2/meters',
+            headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
+        })
+            .then((Response) => {
+                expect(Response.status).to.eq(200)
+                let res = Response.body
+                cy.log(res)
+                var strMeterObj = '/users/' + uuid + '/homes/1/gws/2/meters/1'
+                var meterObj = res[strMeterObj]
+                meterToken = meterObj.token
+                cy.log(meterToken)
             })
     })
 
@@ -184,15 +203,13 @@ describe("GB download", () => {
         cy.task('downloads', 'cypress/downloads').then(after => {
             console.log(after)
             console.log(strObj)
-            // cy.readFile("cypress/downloads/" + after).should('contain', 'Green Button Data File. Meter: 10057:_767106007_76710600_07671060015459718001_AMR')
-            // cy.readFile("cypress/downloads/" + after).should('contain', '<espi:ReadingType><espi:accumulationBehaviour>4</espi:accumulationBehaviour><espi:commodity>7</espi:commodity><espi:dataQualifier>12</espi:dataQualifier><espi:defaultQuality>17</espi:defaultQuality><espi:flowDirection>1</espi:flowDirection><espi:intervalLength>3600</espi:intervalLength><espi:kind>58</espi:kind><espi:phase>769</espi:phase><espi:powerOfTenMultiplier>-3</espi:powerOfTenMultiplier><espi:timeAttribute>0</espi:timeAttribute><espi:uom>119</espi:uom></espi:ReadingType>')
-        //    for (let index = 0; index < arrValues.length; index++) {
-        //        const element = arrValues[index];
-        //        console.log(element)
-               cy.readFile("cypress/downloads/" + after).should('contain', strObj)
-        //    }
-        //     // cy.readFile("cypress/downloads/"+after).should('contain', '<espi:IntervalBlock><espi:interval><espi:duration>86400</espi:duration><espi:start>1654750800</espi:start></espi:interval><espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>86400</espi:duration><espi:start>1654750800</espi:start></espi:timePeriod><espi:value>111300</espi:value></espi:IntervalReading></espi:IntervalBlock></content><published>2022-07-26T13:24:52.275Z</published><updated>2022-07-26T13:24:52.275Z</updated></entry><entry><id>urn:uuid:012bc7b5-2bde-3a8e-9874-31f27f3bf0e0</id><link href="http://amerenuatapi.bidgely.com/Customer/39977728884556036/UsagePoint/9056348086580949057/MeterReading/5429490602267981274/IntervalBlock/5534999940292988461" rel="self" type="espi-entry/IntervalBlock"/><link href="http://amerenuatapi.bidgely.com/Customer/39977728884556036/UsagePoint/9056348086580949057/MeterReading/5429490602267981274/IntervalBlock" rel="up" type="espi-feed/IntervalBlock"/><title>IntervalBlock ID: 3582119876618863370; Start: 2022-06-10T05:00-05:00[CST6CDT]</title><content><espi:IntervalBlock><espi:interval><espi:duration>86400</espi:duration><espi:start>1654837200</espi:start></espi:interval><espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>86400</espi:duration><espi:start>1654837200</espi:start></espi:timePeriod><espi:value>111300</espi:value></espi:IntervalReading></espi:IntervalBlock>')
-        //     //   expect(after.length).to.be.eq(before.length +1)  
+            cy.readFile("cypress/downloads/" + after).then(fileToRead => {
+                cy.wrap(fileToRead).should('contain', 'Green Button Data File. Meter: ' + meterToken)
+                cy.wrap(fileToRead).should('contain', '<espi:ReadingType><espi:accumulationBehaviour>4</espi:accumulationBehaviour><espi:commodity>1</espi:commodity><espi:dataQualifier>12</espi:dataQualifier><espi:defaultQuality>17</espi:defaultQuality><espi:flowDirection>1</espi:flowDirection><espi:intervalLength>3600</espi:intervalLength><espi:kind>12</espi:kind><espi:phase>769</espi:phase><espi:powerOfTenMultiplier>0</espi:powerOfTenMultiplier><espi:timeAttribute>0</espi:timeAttribute><espi:uom>72</espi:uom></espi:ReadingType>')
+                cy.wrap(fileToRead).should('contain', strObj)
+
+            })
+            //   expect(after.length).to.be.eq(before.length +1)  
         })
     })
 
--- cypress/e2e/Ameren/GbDownloadAMRE.cy.js
+++ cypress/e2e/Ameren/GbDownloadAMRE.cy.js
@@ -10,12 +10,15 @@ describe("GB download", () => {
     var billingEndTs
     var strStartDate
     var strEndDate
+    var strMinDate
+    var strMaxDate
     var strNewStartDate
     var newEpochStartTs
     var strNewEndDate
     var newEpochEndTs
     var arrValues = []
     var strObj = ''
+    var meterToken
     const uuid = 'ac7951ce-0474-4946-be0c-0539d1d767bc'
     var bearerToken
     var userHash
@@ -26,15 +29,15 @@ describe("GB download", () => {
             bearerToken = token
             cy.request({
                 method: 'GET',
-                url: baseUrl+'/v2.0/user-auth/cipher?user-id='+uuid+'&pilot-id='+pilotData.pilotId,
-                headers: { 'Authorization': 'Bearer '+bearerToken }, timeout: 30000
+                url: baseUrl + '/v2.0/user-auth/cipher?user-id=' + uuid + '&pilot-id=' + pilotData.pilotId,
+                headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
             })
                 .then((Response) => {
                     expect(Response.status).to.eq(200)
                     let res = Response.body
                     cy.log(res.payload)
                     userHash = res.payload
-                    cy.visit(pilotData.url+"dashboard?user-hash="+res.payload)
+                    cy.visit(pilotData.url + "dashboard?user-hash=" + res.payload)
                 })
         })
     })
@@ -43,40 +46,47 @@ describe("GB download", () => {
         cy.log(bearerToken)
         cy.request({
             method: 'GET',
-            url: baseUrl+'/billingdata/users/'+uuid+'/homes/1/utilitydata?t0=1&t1=1906986799&measurementType=ELECTRIC',
-            headers: { 'Authorization': 'Bearer '+bearerToken }, timeout: 30000
+            url: baseUrl + '/billingdata/users/' + uuid + '/homes/1/utilitydata?t0=1&t1=1906986799&measurementType=ELECTRIC',
+            headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
         })
             .then((Response) => {
                 expect(Response.status).to.eq(200)
                 let res = Response.body
                 cy.log(res)
                 var objLength = Object.keys(res).length;
-                var firstKey = Object.keys(res)[objLength-3];
+                var firstKey = Object.keys(res)[objLength - 3];
                 let objData = res[firstKey]
                 cy.log(objData)
+
+                var objFirst = Object.keys(res)[0]
+                var firstObjData = res[objFirst]
+                var firstBillingStartTs = firstObjData['billingStartTs']
+                var firstStartTs = new Date(firstBillingStartTs * 1000);
+                strMinDate = objGenericPage.changDateFormat(firstStartTs.toDateString())
+
                 billingEndTs = objData['billingEndTs']
                 billingStartTs = objData['billingStartTs']
                 cy.log(billingStartTs)
                 cy.log(billingEndTs)
-                var startTs = new Date(billingStartTs*1000);
-                var endTs = new Date(billingEndTs*1000);
+                var startTs = new Date(billingStartTs * 1000);
+                var endTs = new Date(billingEndTs * 1000);
                 endTs.setDate(endTs.getDate() - 1)
                 cy.log(startTs.toLocaleString())
                 cy.log(endTs.toDateString())
                 strStartDate = objGenericPage.changDateFormat(startTs.toDateString())
                 strEndDate = objGenericPage.changDateFormat(endTs.toDateString())
-                cy.log('Start date - '+strStartDate)
+                cy.log('Start date - ' + strStartDate)
                 cy.wrap(strStartDate).as('strStartDate');
 
-                var newStartTs = new Date(billingEndTs*1000);
+                var newStartTs = new Date(billingEndTs * 1000);
                 newStartTs.setDate(newStartTs.getDate() - 10)
                 strNewStartDate = objGenericPage.changDateFormat(newStartTs.toDateString())
-                newEpochStartTs = newStartTs.getTime()/1000
+                newEpochStartTs = newStartTs.getTime() / 1000
 
-                var newEndTs = new Date(billingEndTs*1000);
-                newEndTs.setDate(newEndTs.getDate() -1)
+                var newEndTs = new Date(billingEndTs * 1000);
+                newEndTs.setDate(newEndTs.getDate() - 1)
                 strNewEndDate = objGenericPage.changDateFormat(newEndTs.toDateString())
-                newEpochEndTs = newEndTs.getTime()/1000
+                newEpochEndTs = newEndTs.getTime() / 1000
             })
     })
 
@@ -115,9 +125,36 @@ describe("GB download", () => {
         objGbDownload.clickExport()
         objGbDownload.checkSuccessMsg('GreenButton data downloaded successfully.')
     })
-    it("Export data - Days", () => {
-        cy.log('Start date - '+strStartDate)
 
+    it("Export data - Calendar Days", () => {
+        cy.contains('Export usage for range of days').click()
+        cy.wait(200)
+        cy.get(objGbDownload.calendarIcon).eq(0).should('be.visible')
+        cy.get(objGbDownload.calendarIcon).eq(1).should('be.visible')
+        
+        cy.get(objGbDownload.calendarIcon).eq(0).click()
+        cy.get('[aria-label="Go to the previous month"]').click()
+        cy.get('[role="presentation"]').eq(10).click()
+
+        cy.get(objGbDownload.calendarIcon).eq(1).click()
+        cy.get('[aria-label="Go to the previous month"]').click()
+        cy.get('[role="presentation"]').eq(11).click()
+        objGbDownload.clickExport()
+        objGbDownload.checkSuccessMsg('GreenButton data downloaded successfully.')
+        cy.wait(500)
+
+        objGbDownload.enterFromDate(strMinDate)
+        objGbDownload.enterToDate(strEndDate)
+        objGbDownload.clickExport()
+        objGbDownload.checkSuccessMsg('Please select a date range that does not exceed one year.')
+
+        objGbDownload.enterFromDate(strStartDate)
+        objGbDownload.enterToDate(strStartDate)
+        objGbDownload.clickExport()
+        objGbDownload.checkSuccessMsg('GreenButton data downloaded successfully.')
+    })
+
+    it("Export data - Days", () => {
         cy.contains('Export usage for range of days').click()
         objGbDownload.enterFromDate('16/09/20')
         objGbDownload.checkErrorMsg('Invalid Date Format')
@@ -131,9 +168,6 @@ describe("GB download", () => {
         objGbDownload.enterToDate(strEndDate)
         objGbDownload.clickExport()
         objGbDownload.checkSuccessMsg('GreenButton data downloaded successfully.')
-        cy.wait(500)
-        cy.get(objGbDownload.calendarIcon).eq(0).should('be.visible')
-        cy.get(objGbDownload.calendarIcon).eq(1).should('be.visible')
     })
 
     it("Check Disabled fields", () => {
@@ -156,8 +190,8 @@ describe("GB download", () => {
     it('Fetch values from RAW data', () => {
         cy.request({
             method: 'GET',
-            url: baseUrl+'/streams/users/'+uuid+'/homes/1/gws/3/meters/1/gb.json?t0='+billingStartTs+'&t1='+newEpochEndTs,
-            headers: { 'Authorization': 'Bearer '+bearerToken }, timeout: 30000
+            url: baseUrl + '/streams/users/' + uuid + '/homes/1/gws/3/meters/1/gb.json?t0=' + billingStartTs + '&t1=' + newEpochEndTs,
+            headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
         })
             .then((Response) => {
                 expect(Response.status).to.eq(200)
@@ -175,11 +209,28 @@ describe("GB download", () => {
                     const duration = element['duration']
                     value = Math.round(value)
                     cy.log(value)
-                    strObj = '<espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>'+duration+'</espi:duration><espi:start>'+time+'</espi:start></espi:timePeriod><espi:value>'+value+'</espi:value></espi:IntervalReading>'
+                    strObj = '<espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>' + duration + '</espi:duration><espi:start>' + time + '</espi:start></espi:timePeriod><espi:value>' + value + '</espi:value></espi:IntervalReading>'
                     arrValues.push(strObj)
                     // console.log(strObj)
 
                 }
+            })
+    })
+
+    it('Fetch values from Meter API', () => {
+        cy.request({
+            method: 'GET',
+            url: baseUrl + '/meta/users/' + uuid + '/homes/1/gws/3/meters',
+            headers: { 'Authorization': 'Bearer ' + bearerToken }, timeout: 30000
+        })
+            .then((Response) => {
+                expect(Response.status).to.eq(200)
+                let res = Response.body
+                cy.log(res)
+                var strMeterObj = '/users/' + uuid + '/homes/1/gws/3/meters/1'
+                var meterObj = res[strMeterObj]
+                meterToken = meterObj.token
+                cy.log(meterToken)
             })
     })
 
@@ -188,17 +239,16 @@ describe("GB download", () => {
             console.log(after)
             console.log(strObj)
             cy.readFile("cypress/downloads/" + after).then(fileToRead => {
-                // cy.log(fileToRead)
-            // cy.readFile("cypress/downloads/" + after).should('contain', 'Green Button Data File. Meter: 10057:_767106007_76710600_07671060015459718001_AMR')
-            // cy.readFile("cypress/downloads/" + after).should('contain', '<espi:ReadingType><espi:accumulationBehaviour>4</espi:accumulationBehaviour><espi:commodity>7</espi:commodity><espi:dataQualifier>12</espi:dataQualifier><espi:defaultQuality>17</espi:defaultQuality><espi:flowDirection>1</espi:flowDirection><espi:intervalLength>3600</espi:intervalLength><espi:kind>58</espi:kind><espi:phase>769</espi:phase><espi:powerOfTenMultiplier>-3</espi:powerOfTenMultiplier><espi:timeAttribute>0</espi:timeAttribute><espi:uom>119</espi:uom></espi:ReadingType>')
-           for (let index = 0; index < arrValues.length; index++) {
-               const element = arrValues[index];
-               console.log(element)
-               cy.wrap(fileToRead).should('contain', element)
-           }
-        })
-        //     // cy.readFile("cypress/downloads/"+after).should('contain', '<espi:IntervalBlock><espi:interval><espi:duration>86400</espi:duration><espi:start>1654750800</espi:start></espi:interval><espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>86400</espi:duration><espi:start>1654750800</espi:start></espi:timePeriod><espi:value>111300</espi:value></espi:IntervalReading></espi:IntervalBlock></content><published>2022-07-26T13:24:52.275Z</published><updated>2022-07-26T13:24:52.275Z</updated></entry><entry><id>urn:uuid:012bc7b5-2bde-3a8e-9874-31f27f3bf0e0</id><link href="http://amerenuatapi.bidgely.com/Customer/39977728884556036/UsagePoint/9056348086580949057/MeterReading/5429490602267981274/IntervalBlock/5534999940292988461" rel="self" type="espi-entry/IntervalBlock"/><link href="http://amerenuatapi.bidgely.com/Customer/39977728884556036/UsagePoint/9056348086580949057/MeterReading/5429490602267981274/IntervalBlock" rel="up" type="espi-feed/IntervalBlock"/><title>IntervalBlock ID: 3582119876618863370; Start: 2022-06-10T05:00-05:00[CST6CDT]</title><content><espi:IntervalBlock><espi:interval><espi:duration>86400</espi:duration><espi:start>1654837200</espi:start></espi:interval><espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>86400</espi:duration><espi:start>1654837200</espi:start></espi:timePeriod><espi:value>111300</espi:value></espi:IntervalReading></espi:IntervalBlock>')
-        //     //   expect(after.length).to.be.eq(before.length +1)  
+                cy.wrap(fileToRead).should('contain', 'Green Button Data File. Meter: ' + meterToken)
+                cy.wrap(fileToRead).should('contain', '<espi:ReadingType><espi:accumulationBehaviour>4</espi:accumulationBehaviour><espi:commodity>1</espi:commodity><espi:dataQualifier>12</espi:dataQualifier><espi:defaultQuality>17</espi:defaultQuality><espi:flowDirection>1</espi:flowDirection><espi:intervalLength>86400</espi:intervalLength><espi:kind>12</espi:kind><espi:phase>769</espi:phase><espi:powerOfTenMultiplier>0</espi:powerOfTenMultiplier><espi:timeAttribute>0</espi:timeAttribute><espi:uom>72</espi:uom></espi:ReadingType>')
+                for (let index = 0; index < arrValues.length; index++) {
+                    const element = arrValues[index];
+                    console.log(element)
+                    cy.wrap(fileToRead).should('contain', element)
+                }
+            })
+            //     // cy.readFile("cypress/downloads/"+after).should('contain', '<espi:IntervalBlock><espi:interval><espi:duration>86400</espi:duration><espi:start>1654750800</espi:start></espi:interval><espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>86400</espi:duration><espi:start>1654750800</espi:start></espi:timePeriod><espi:value>111300</espi:value></espi:IntervalReading></espi:IntervalBlock></content><published>2022-07-26T13:24:52.275Z</published><updated>2022-07-26T13:24:52.275Z</updated></entry><entry><id>urn:uuid:012bc7b5-2bde-3a8e-9874-31f27f3bf0e0</id><link href="http://amerenuatapi.bidgely.com/Customer/39977728884556036/UsagePoint/9056348086580949057/MeterReading/5429490602267981274/IntervalBlock/5534999940292988461" rel="self" type="espi-entry/IntervalBlock"/><link href="http://amerenuatapi.bidgely.com/Customer/39977728884556036/UsagePoint/9056348086580949057/MeterReading/5429490602267981274/IntervalBlock" rel="up" type="espi-feed/IntervalBlock"/><title>IntervalBlock ID: 3582119876618863370; Start: 2022-06-10T05:00-05:00[CST6CDT]</title><content><espi:IntervalBlock><espi:interval><espi:duration>86400</espi:duration><espi:start>1654837200</espi:start></espi:interval><espi:IntervalReading><espi:ReadingQuality><espi:quality>17</espi:quality></espi:ReadingQuality><espi:timePeriod><espi:duration>86400</espi:duration><espi:start>1654837200</espi:start></espi:timePeriod><espi:value>111300</espi:value></espi:IntervalReading></espi:IntervalBlock>')
+            //     //   expect(after.length).to.be.eq(before.length +1)  
         })
     })
 
